cb2d416e28b660e4f2166dc2af58c97dfca64634
James Suescun <james.steven2318@gmail.com>
2022-04-19 19:24:18 -0400

    update stylesheet

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
 
-import Header from "./components/Header/Header.js";
-import List from "./components/List/List.js";
-import Completed from "./components/Completed/Completed.js";
 import { toDoListData } from "./components/data.js";
 import { useState } from "react";
+import "./style.css";
 
 function App() {
   const [todoList, setTodoList] = useState(toDoListData);
-  const [doneList, setDoneList] = useState(["test"]);
+  const [input, setInput] = useState("");
+  
+  const handleChange = e => {
+    setInput(e.target.value)
+  }
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(input);
+    setInput("");
+}
+  const completedItem = (e) => {
+    // setTodoList(
+    //   todoList.map(
+    //     task => task.id !== id 
+    //     ? task.completed = !task.completed
+    //     : task)
+    // )
+
+  }
+
+  const removeItem = () => {
+
+  }
   const displayList = (item, idx) => {
-    return <li key={idx}> { item.title } </li> 
+    return (
+      <>
+    
+     { item.completed 
+     ? <li key={idx}> { item.title } <button onClick={removeItem}> Remove</button></li> 
+     : <li key={idx}> { item.title } <button onClick={completedItem}> Completed</button></li> 
+    }
+   
+     </>
+    )
   }
 
   const handleSubmit = (item) => {
-    setTodoList([...todoList, {title: item}])
+    setTodoList([...todoList, {title: item, completed: false}])
   }
 
   return (
     <section>
-      <Header handleSubmit={ handleSubmit } />
-      <List data={ todoList } displayList={ displayList }/>
-      <Completed data={ doneList } displayList={ displayList }/> 
+      <header>
+            <h1>My To Do List: </h1>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="to-do-item">New Item</label>
+                <input type="text" id="to-do-item" name="to-do-item" value={input} onChange={ handleChange }/>
+            </form>
+        </header>
+      <article>
+        <h3>TO DO</h3>
+        {
+          todoList.filter(x => !x.completed).map(displayList)
+        }
+      </article>
+      <article>
+        <h3>COMPLETED</h3>
+        {
+          todoList.filter(x => x.completed).map(displayList)
+        }
+      </article>
     </section>
   );
 }
